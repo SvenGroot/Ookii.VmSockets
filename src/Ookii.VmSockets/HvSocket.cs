3d20ebaa3d35c27e3efc1c5662d580d67878bd72
Sven Groot <svgroot@microsoft.com>
2024-04-04 16:16:27 -0700

    XML comments and exceptions.

--- src/Ookii.VmSockets/HvSocket.cs
+++ src/Ookii.VmSockets/HvSocket.cs
@@ -2,26 +2,98 @@
 
 namespace Ookii.VmSockets;
 
+/// <summary>
+/// Provides constants and helper methods for working with Hyper-V sockets.
+/// </summary>
 public static class HvSocket
 {
+    /// <summary>
+    /// The address family for Hyper-V sockets, also known as <c>AF_HYPERV</c>.
+    /// </summary>
     public const AddressFamily AddressFamily = (AddressFamily)34;
 
+    /// <summary>
+    /// The size of the <c>SOCKADDR_HV</c> structure, used to describe Hyper-V socket addresses.
+    /// </summary>
     public const int SocketAddressSize = 36;
 
+    /// <summary>
+    /// Broadcast address for Hyper-V sockets.
+    /// </summary>
     public static readonly Guid Broadcast = new(0xFFFFFFFF, 0xFFFF, 0xFFFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF);
 
+    /// <summary>
+    /// Wildcard address for children.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    ///   Listeners should bind to this VM ID to accept connections from its children.
+    /// </para>
+    /// </remarks>
     public static readonly Guid Children = new(0x90DB8B89, 0x0D35, 0x4F79, 0x8C, 0xE9, 0x49, 0xEA, 0x0A, 0xC8, 0xB7, 0xCD);
 
+    /// <summary>
+    /// Loopback address for Hyper-V sockets.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    ///   Using this VmId connects to the same partition as the connector.
+    /// </para>
+    /// </remarks>
     public static readonly Guid Loopback = new(0xE0E16197, 0xDD56, 0x4A10, 0x91, 0x95, 0x5E, 0xE7, 0xA1, 0x55, 0xA8, 0x38);
 
+    /// <summary>
+    /// Parent address for Hyper-V sockets.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    ///   Using this VmId connects or listens to the parent partition of the connector. The parent
+    ///   of a virtual machine is its host. The parent of a hosted silo is the VM's host.
+    /// </para>
+    /// </remarks>
     public static readonly Guid Parent = new(0xA42E7CDA, 0xD03F, 0x480C, 0x9C, 0xC2, 0xA4, 0xDE, 0x20, 0xAB, 0xB8, 0x78);
 
+    /// <summary>
+    /// Address of a silo's host partition.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    ///   The silo host of a hosted silo is the utility VM. The silo host of a silo on a physical
+    ///   host is the physical host.
+    /// </para>
+    /// </remarks>
     public static readonly Guid SiloHost = new(0x36BD0C5C, 0x7276, 0x4223, 0x88, 0xBA, 0x7D, 0x03, 0xB6, 0x54, 0xC5, 0x68);
 
+    /// <summary>
+    /// Wildcard address for Hyper-V sockets.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    ///   Listeners should bind to this VmId to accept connection from all partitions.
+    /// </para>
+    /// </remarks>
     public static readonly Guid Wildcard = Guid.Empty;
 
+    /// <summary>
+    /// Template to facilitate a mapping between the service ID of a Hyper-V socket and the port
+    /// number of a Linux VSock socket.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    ///   To create a service ID for a VSock port number, use the <see cref="CreateVSockServiceId"/>
+    ///   method, or use the <see cref="HvSocketEndPoint(Guid, int)"/> constructor.
+    /// </para>
+    /// </remarks>
     public static readonly Guid VSockTemplate = new(0x00000000, 0xFACB, 0x11E6, 0xBD, 0x58, 0x64, 0x00, 0x6A, 0x79, 0x86, 0xD3);
 
+    /// <summary>
+    /// Maps the port number of a Linux VSock socket to a service ID for a Hyper-V socket.
+    /// </summary>
+    /// <param name="port">The VSock port number. Valid values are between 0 and 0x7fffffff.</param>
+    /// <returns>The mapped service ID.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="port"/> is less than 0.
+    /// </exception>
     public static Guid CreateVSockServiceId(int port)
     {
         if (port < 0)
--- src/Ookii.VmSockets/HvSocketEndPoint.cs
+++ src/Ookii.VmSockets/HvSocketEndPoint.cs
@@ -3,33 +3,142 @@ using System.Net.Sockets;
 
 namespace Ookii.VmSockets;
 
+/// <summary>
+/// Represents a Hyper-V socket (hvsocket) endpoint as a VM ID and a service ID.
+/// </summary>
+/// <remarks>
+/// <para>
+///   Hyper-V sockets can be used between a host and a guest virtual machine or a silo. It
+///   provides interoperability with VSock sockets for guest partitions running Linux.
+/// </para>
+/// </remarks>
+/// <threadsafety instance="false" static="true" />
+/// <seealso href="https://learn.microsoft.com/en-us/virtualization/hyper-v-on-windows/user-guide/make-integration-service"/>
 public class HvSocketEndPoint : EndPoint
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HvSocketEndPoint"/> class.
+    /// </summary>
+    /// <param name="vmId">
+    /// The ID of the virtual machine, or one of the well-known IDs defined in the
+    /// <see cref="HvSocket"/> class.
+    /// </param>
+    /// <param name="serviceId">The service ID.</param>
+    /// <remarks>
+    /// <para>
+    ///   To connect to a Linux guest that is using VSock sockets, use the
+    ///   <see cref="HvSocketEndPoint(Guid, int)"/> constructor.
+    /// </para>
+    /// </remarks>
     public HvSocketEndPoint(Guid vmId, Guid serviceId)
     {
         VmId = vmId;
         ServiceId = serviceId;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HvSocketEndPoint"/> class using a VSock port
+    /// number.
+    /// </summary>
+    /// <param name="vmId">
+    /// The ID of the virtual machine, or one of the well-known IDs defined in the
+    /// <see cref="HvSocket"/> class.
+    /// </param>
+    /// <param name="port">The VSock port number. Valid values are between 0 and 0x7fffffff.</param>
+    /// <remarks>
+    /// <para>
+    ///   This constructor can be used to create an endpoint that connects to a Linux guest that is
+    ///   using VSock sockets.
+    /// </para>
+    /// </remarks>
+    /// <seealso cref="HvSocket.CreateVSockServiceId(int)"/>
     public HvSocketEndPoint(Guid vmId, int port)
     {
         VmId = vmId;
         ServiceId = HvSocket.CreateVSockServiceId(port);
     }
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <value>
+    /// The value of AF_HYPERV.
+    /// </value>
     public override AddressFamily AddressFamily => HvSocket.AddressFamily;
 
+    /// <summary>
+    /// Gets or sets the virtual machine ID.
+    /// </summary>
+    /// <value>
+    /// The ID of the virtual machine, or one of the well-known IDs defined in the
+    /// <see cref="HvSocket"/> class.
+    /// </value>
+    public Guid VmId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the service ID.
+    /// </summary>
+    /// <value>
+    /// The service ID.
+    /// </value>
+    /// <remarks>
+    /// <para>
+    ///   To create a service ID that represents a VSock port number, use the
+    ///   <see cref="HvSocket.CreateVSockServiceId(int)" qualifyHint="true"/> method, or the
+    ///   <see cref="HvSocketEndPoint(Guid, int)"/> constructor.
+    /// </para>
+    /// </remarks>
+    public Guid ServiceId { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="socketAddress"><inheritdoc/></param>
+    /// <returns><inheritdoc/></returns>
+    /// <remarks>
+    /// <para>
+    ///   The <paramref name="socketAddress"/> parameter must use the format of the
+    ///   <c>SOCKADDR_HV</c> structure.
+    /// </para>
+    /// </remarks>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="socketAddress"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// The AddressFamily of <paramref name="socketAddress"/> is not <c>AF_HYPERV</c>, or the size
+    /// of <paramref name="socketAddress"/> is less than <see cref="HvSocket.SocketAddressSize"/>.
+    /// </exception>
     public override EndPoint Create(SocketAddress socketAddress)
     {
+        if (socketAddress == null)
+        {
+            throw new ArgumentNullException(nameof(socketAddress));
+        }
+
+        if (socketAddress.Family != AddressFamily)
+        {
+            throw new ArgumentException(Properties.Resources.InvalidAddressFamily, nameof(socketAddress));
+        }
+
+        if (socketAddress.Size < HvSocket.SocketAddressSize)
+        {
+            throw new ArgumentException(Properties.Resources.InvalidAddressSize, nameof(socketAddress));
+        }
+
         var vmId = socketAddress.ReadGuid(4);
         var serviceId = socketAddress.ReadGuid(20);
         return new HvSocketEndPoint(vmId, serviceId);
     }
 
-    public Guid VmId { get; set; }
-
-    public Guid ServiceId { get; set; }
-
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns><inheritdoc/></returns>
+    /// <remarks>
+    /// <para>
+    /// The returned <see cref="SocketAddress"/> will use the format of the <c>SOCKADDR_HV</c> structure.
+    /// </para>
+    /// </remarks>
     public override SocketAddress Serialize()
     {
         var address = new SocketAddress(AddressFamily, HvSocket.SocketAddressSize);
