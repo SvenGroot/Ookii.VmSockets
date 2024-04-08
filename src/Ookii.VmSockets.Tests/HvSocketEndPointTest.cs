a0d877b99f3505262dd64ecd33b2fa1f65ecdbae
Sven Groot <svgroot@microsoft.com>
2024-04-08 14:32:14 -0700

    Add tests for Create and Serialize

--- src/Ookii.VmSockets.Tests/HvSocketEndPointTest.cs
+++ src/Ookii.VmSockets.Tests/HvSocketEndPointTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Networking.WinSock;
+using Windows.Win32.System.Hypervisor;
 
 namespace Ookii.VmSockets.Tests;
 
@@ -17,5 +20,44 @@ public class HvSocketEndPointTest
         endpoint = new HvSocketEndPoint(vmId, 1234);
         Assert.AreEqual(vmId, endpoint.VmId);
         Assert.AreEqual(new Guid("000004D2-facb-11e6-bd58-64006a7986d3"), endpoint.ServiceId);
+    }
+
+    [TestMethod]
+    public void TestSerialize()
+    {
+        var vmId = Guid.NewGuid();
+        var serviceId = Guid.NewGuid();
+        var endpoint = new HvSocketEndPoint(vmId, serviceId);
+        var socketAddress = endpoint.Serialize();
+        var buffer = socketAddress.GetBuffer();
+        var expected = new SOCKADDR_HV()
+        {
+            Family = (ADDRESS_FAMILY)PInvoke.AF_HYPERV,
+            Reserved = 0,
+            VmId = vmId,
+            ServiceId = serviceId,
+        };
+
+        ReadOnlySpan<SOCKADDR_HV> span;
+        unsafe
+        {
+            span = new ReadOnlySpan<SOCKADDR_HV>(&expected, 1);
+        }
+
+        var expectedBuffer = MemoryMarshal.AsBytes(span);
+        CollectionAssert.AreEqual(expectedBuffer.ToArray(), buffer.ToArray());
+    }
+
+    [TestMethod]
+    public void TestCreate()
+    {
+        var vmId = Guid.NewGuid();
+        var serviceId = Guid.NewGuid();
+        var endpoint = new HvSocketEndPoint(vmId, serviceId);
+        var socketAddress = endpoint.Serialize();
+        var endpoint2 = new HvSocketEndPoint(Guid.Empty, Guid.Empty);
+        endpoint2 = (HvSocketEndPoint)endpoint2.Create(socketAddress);
+        Assert.AreEqual(endpoint.VmId, endpoint2.VmId);
+        Assert.AreEqual(endpoint.ServiceId, endpoint2.ServiceId);
     }
 }
---
+++ src/Ookii.VmSockets.Tests/SocketAddressHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace Ookii.VmSockets.Tests;
+
+static class SocketAddressHelper
+{
+    public static Memory<byte> GetBuffer(this SocketAddress address)
+    {
+#if NET8_0_OR_GREATER
+        return address.Buffer;
+#else
+        byte[] buffer = new byte[address.Size];
+        for (int i = 0; i < address.Size; i++)
+        {
+            buffer[i] = address[i];
+        }
+
+        return buffer.AsMemory();
+#endif
+    }
+}
